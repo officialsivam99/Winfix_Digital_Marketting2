2812d56ce90a227254dd220ddc5e2d1168b73b5f
Shivam19990 <officialsivam99@gmail.com>
2025-09-05 16:16:12 +0530

    Deployed till here

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  createBrowserRouter,
-  RouterProvider,
+  HashRouter,
+  Routes,
+  Route
 } from "react-router-dom";
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
-import WebDevelopmentService from './components/WebDevelopmentService';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import WebDevelopmentService from './components/WebDevelopmentService';
 import SocialMediaMarketing from './components/SocialMediaMarketing';
 import SearchEngineOptimization from './components/SearchEngineOptimization';
 import PayPerClickMarketing from './components/PayPerClickMarketing';
@@ -19,61 +20,29 @@ import Terms from './components/Terms';
 import ReturnRefund from './components/ReturnRefund';
 
 
-let pages = [
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "/home",
-    element: <App />
-  },
-  {
-    path: "/WebDevelopmentService",
-    element: <WebDevelopmentService />
-  },
-  {
-    path: "/SocialMediaMarketing",
-    element: <SocialMediaMarketing />
-  },
-  {
-    path: "/SearchEngineOptimization",
-    element: <SearchEngineOptimization />
-  },
-  {
-    path: "/PayPerClickMarketing",
-    element: <PayPerClickMarketing />
-  },
-  {
-    path: "/EmailMarketing",
-    element: <EmailMarketing />
-  },
-  {
-    path: "/AnalyticsDashboard",
-    element: <AnalyticsDashboard />
-  },
-  {
-    path: "/PrivacyPolicy",
-    element: <PrivacyPolicy />
-  },
-  {
-    path: "/Terms",
-    element: <Terms />
-  },
-  {
-    path: "/ReturnRefund",
-    element: <ReturnRefund />
-  },
-  {
-    path: "*",
-    element: <div style={{textAlign:'center',padding:'80px 0'}}><h1>404 - Page Not Found</h1><p>The page you are looking for does not exist.</p></div>
-  }
-]
-const router = createBrowserRouter(pages);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<App />} />
+        <Route path="/WebDevelopmentService" element={<WebDevelopmentService />} />
+        <Route path="/SocialMediaMarketing" element={<SocialMediaMarketing />} />
+        <Route path="/SearchEngineOptimization" element={<SearchEngineOptimization />} />
+        <Route path="/PayPerClickMarketing" element={<PayPerClickMarketing />} />
+        <Route path="/EmailMarketing" element={<EmailMarketing />} />
+        <Route path="/AnalyticsDashboard" element={<AnalyticsDashboard />} />
+        <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+        <Route path="/Terms" element={<Terms />} />
+        <Route path="/ReturnRefund" element={<ReturnRefund />} />
+        <Route path="*" element={
+          <div style={{textAlign:'center',padding:'80px 0'}}>
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+          </div>
+        } />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
